61f846f6434bc7423ab2f8734f0da488ef40671e
Ayush Kapoor <stringakaak@gmail.com>
2023-05-30 14:38:29 +0530

    added projects information

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,27 +1,22 @@
-import Image from "next/image";
 import aldiLogo from "../assets/aldi-logo.png";
 import aldiLogoBig from "../assets/aldifest_2023.webp";
+import stageverseLogoBig from "../assets/blogAppScreenShot.png";
+import blogAppSS from "../assets/blogAppScreenShot2.png";
+import stageverseLogo from "../assets/stageverse-logo.svg";
+import Card from "./ui/card";
 export default function Projects() {
   return (
     <div className="flex flex-col gap-8 w-full max-w-4xl">
-      <div className="font-medium text-2xl">Projects</div>
+      <div className="font-medium text-2xl">Recent Projects</div>
       <div className="flex flex-nowrap gap-8 ">
-        <div className="w-[300px] h-[400px] bg-[#3AC9F7] flex flex-col justify-between items-center rounded-xl py-16">
-          <Image
-            src={aldiLogo}
-            width={0}
-            height={0}
-            className="w-44"
-            alt="aldifest_logo"
-          ></Image>
-          <Image
-            src={aldiLogoBig}
-            width={0}
-            height={0}
-            className="w-56"
-            alt="aldifest_logo"
-          ></Image>
-        </div>
+        <Card topImg={aldiLogo} bottomImg={aldiLogoBig} bgColor="#3AC9F7" />
+        <Card topTitle="Blog Website" bottomImg={blogAppSS} bgColor="#EF85EF" />
+        <Card
+          topImg={stageverseLogo}
+          bottomImg={stageverseLogoBig}
+          bgColor="linear-gradient( #6e80ef, #61dfc7)"
+          bottomImageClassName="scale-[150%] relative top-8"
+        />
       </div>
     </div>
   );
---
+++ src/app/components/ui/card.tsx
@@ -0,0 +1,49 @@
+import Image, { StaticImageData } from "next/image";
+
+interface CardProps {
+  title?: string;
+  duration?: string;
+  desc?: string[];
+  topTitle?: string;
+  topImg?: StaticImageData | string;
+  bottomImg: StaticImageData | string;
+  bgColor: string;
+  bottomImageClassName?: string;
+}
+
+export default function Card({
+  title,
+  duration,
+  desc,
+  topTitle = "Project Title",
+  topImg,
+  bottomImg,
+  bgColor,
+  bottomImageClassName,
+}: CardProps) {
+  return (
+    <div
+      style={{ background: bgColor }}
+      className={`w-[300px] h-[400px] gap-10 flex flex-col justify-between items-center rounded-xl py-16 overflow-hidden`}
+    >
+      {topImg ? (
+        <Image
+          src={topImg}
+          width={0}
+          height={0}
+          className="w-44 "
+          alt="aldifest_logo"
+        ></Image>
+      ) : (
+        <div className="text-white text-xl">{topTitle}</div>
+      )}
+      <Image
+        src={bottomImg}
+        width={0}
+        height={0}
+        className={bottomImageClassName ?? "w-56 "}
+        alt="aldifest_logo"
+      ></Image>
+    </div>
+  );
+}
