5cf548c77b1f236299da51aea826643cc4fa3d13
ayush391 <ayush.kapoor@raftlabs.co>
2025-11-02 14:30:22 +0530

    Add react-scan auto script to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />
+      </head>
       <body className="dark:bg-neutral-700 dark:text-white">{children}</body>
     </html>
   );
